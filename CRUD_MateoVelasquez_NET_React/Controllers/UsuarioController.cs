b6d79c7b89d36b9a6fe6ebde8d1890b1b33e7fc9
Mateo Nicolas Velasquez Gallardo <mateo.velasquez@udla.edu.ec>
2023-10-02 11:31:56 -0500

    Implementación del Controlador de Usuario, operaciones CRUD

---
+++ CRUD_MateoVelasquez_NET_React/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CRUD_MateoVelasquez_NET_React.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MateoVelasquez_NET_React.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly CrudContext _dbcontext;
+
+        public UsuarioController(CrudContext context) {
+        
+            _dbcontext = context;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+             List<Usuario> lista = await _dbcontext.Usuarios.OrderByDescending(c => c.Id).ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] Usuario request)
+        {
+            await _dbcontext.Usuarios.AddAsync(request);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK,"ok");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] Usuario request)
+        {
+            _dbcontext.Usuarios.Update(request);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar (int id)
+        {
+            Usuario usuario = _dbcontext.Usuarios.Find(id);
+
+            _dbcontext.Usuarios.Remove(usuario);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+    }
+}
