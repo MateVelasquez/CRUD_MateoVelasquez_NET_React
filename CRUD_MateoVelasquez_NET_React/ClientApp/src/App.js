1b4f8bafe42c31d3bb7c8088ddaa724b67cd925b
Mateo Nicolas Velasquez Gallardo <mateo.velasquez@udla.edu.ec>
2023-10-02 12:07:25 -0500

    Implementación de las vistas de React de las tablas de Usuario y sus respectivas columnas

--- CRUD_MateoVelasquez_NET_React/ClientApp/src/App.js
+++ CRUD_MateoVelasquez_NET_React/ClientApp/src/App.js
@@ -1,1 +1,26 @@
+import { Col, Container, Row, Card, CardHeader, CardBody, Button } from "reactstrap"
+import TablaUsuario from "./componentes/TablaUsuario"
 
+const App = () => {
+
+    return (
+        <Container>
+            <Row classname="mt-5">
+                <Col sm="12">
+                    <Card>
+                        <CardHeader>
+                            <h5>Lista de Usuarios</h5>
+                        </CardHeader>
+                        <CardBody>
+                            <Button size="sm" color="success">Nuevo Usuario</Button>
+                            <hr></hr>
+                            <TablaUsuario/>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default App;
---
+++ CRUD_MateoVelasquez_NET_React/ClientApp/src/componentes/TablaUsuario.js
@@ -0,0 +1,26 @@
+﻿import { Table } from "reactstrap"
+
+const TablaUsuario = () => {
+
+    return (
+
+        <Table stripped responsive>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Pais</th>
+                    <th>Ciudad</th>
+                    <th>Teléfono</th>
+                    <th>Dirección</th>
+                    <th>Código Postal</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </Table>
+    )
+}
+
+export default TablaUsuario
